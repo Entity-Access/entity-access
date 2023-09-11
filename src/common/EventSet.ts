2ac3ca20fea2213b2041189a50c20dc03d388c55
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-09-11 17:58:47 +0530

    Added Events for ChangeSet

---
+++ src/common/EventSet.ts
@@ -0,0 +1,50 @@
+import CustomEvent from "./CustomEvent.js";
+
+export interface IEventSetArgs<T,TArg> {
+    target: T;
+    detail: TArg;
+}
+
+export default class EventSet<TArg, Target = any> {
+
+    public listeners: Set<((x: IEventSetArgs<Target, TArg>) => any)>;
+
+    constructor(private readonly owner: Target) {
+
+    }
+
+    listen(listener: (x: IEventSetArgs<Target, TArg>) => any) {
+        this.listeners ??= new Set();
+        this.listeners.add(listener);
+        return {
+            [Symbol.dispose]: () => this.listeners.delete(listener)
+        };
+    }
+
+    listenOnce(listener: (x: IEventSetArgs<Target, TArg>) => any) {
+        this.listeners ??= new Set();
+        const old = listener;
+        listener = (x) => {
+            old(x);
+            this.listeners.delete(listener);
+        };
+        this.listeners.add(listener);
+    }
+
+    remove(listener: (x: IEventSetArgs<Target, TArg>) => any) {
+        this.listeners.delete(listener);
+    }
+
+    dispatch(detail: TArg) {
+        if (!this.listeners) {
+            return detail;
+        }
+        const arg = { target: this.owner, detail };
+        for (const iterator of this.listeners.values()) {
+            iterator(arg);
+        }
+        return detail;
+    }
+
+
+}
