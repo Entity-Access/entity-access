a9e6ea3556f6e51a467f5c03d3d4666d8cd1b4d4
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-07-23 17:47:16 +0530

    Added unit tests

---
+++ src/tests/expressions/simple/parse-complex.ts
@@ -0,0 +1,20 @@
+import assert from "assert";
+import Sql from "../../../sql/Sql.js";
+import QueryCompiler from "../../../compiler/QueryCompiler.js";
+
+export default function () {
+
+    const compiler = QueryCompiler.instance;
+
+    const name = "Akash";
+
+    let r = compiler.execute({ name }, (p) => (x) => x.id ? 1 : -1);
+    assert.equal("(CASE WHEN x.id THEN 1 ELSE -1 END)", r.text);
+
+    r = compiler.execute({ name }, (p) => (x) => x.id ? 1 : -(5*1));
+    assert.equal("(CASE WHEN x.id THEN 1 ELSE (-5 * 1) END)", r.text);
+
+
+}
+
+type KeyCode = { name: string, code: number, key: string };
