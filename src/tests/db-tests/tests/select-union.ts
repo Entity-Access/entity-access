833f8e6279756ddb5ed9c65590a3f67fffe9645e
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-04-25 12:59:42 +0530

    signature fixed

--- src/tests/db-tests/tests/select-union.ts
+++ src/tests/db-tests/tests/select-union.ts
@@ -17,7 +17,7 @@ export default async function(this: TestConfig) {
     const pq = context.productCategories.where({ headPhoneCategory }, (p) => (x) => x.categoryID === p.headPhoneCategory);
 
     const r = pq
-        .unionAll(context.productCategories.where({ maleClothesCategory }, (p) => (x) => x.category.parentID === p.maleClothesCategory))
+        .unions(context.productCategories.where({ maleClothesCategory }, (p) => (x) => x.category.parentID === p.maleClothesCategory))
         .trace(console.log);
 
     const n = await r.count();
