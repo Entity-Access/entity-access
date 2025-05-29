472c07098f921147928a3199e0e0dfc1b898d83a
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-05-29 21:32:08 +0530

    query fixed

--- src/drivers/sql-server/SqlServerDriver.ts
+++ src/drivers/sql-server/SqlServerDriver.ts
@@ -156,8 +156,8 @@ export class SqlServerConnection extends BaseConnection {
                     ELSE '() => ' + COLUMN_DEFAULT
                 END as [default]
                 FROM INFORMATION_SCHEMA.COLUMNS
-            WHERE TABLE_SCHEMA = $0
-            AND TABLE_NAME = $1
+            WHERE TABLE_SCHEMA = $
+            AND TABLE_NAME = $
         `;
         return this.executeQuery({ text, values: [schema, table] });
     }
